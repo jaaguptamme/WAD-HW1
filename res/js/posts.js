92af408d294734edad3aa5c382365edbfc364c4b
Jaagup Tamme <jaaguptamme@gmail.com>
2023-10-29 19:18:40 +0200

    added comments

--- res/js/posts.js
+++ res/js/posts.js
@@ -1,70 +1,70 @@
-function toggleDropDown(){
-    let DropDown=document.getElementById("dropDown");
-    if(DropDown.style.display!="none"){
+function toggleDropDown(){//function that changes the state of icon dropdown 
+    let DropDown=document.getElementById("dropDown");//Get that div by id
+    if(DropDown.style.display!="none"){//If its shown, then hide
         DropDown.style.display="none";
-    }else{
+    }else{//Otherwise show it
         DropDown.style.display="block";
     }
 }
-
-function addPostToPage(postJSON){
-    let postsDiv = document.getElementById("Posts");
-    let newDiv = document.createElement("div");
-    let topRowDiv=document.createElement("div");
-    topRowDiv.classList.add("topRow");
+//Function that adds a single post to end of index page
+function addPostToPage(postJSON){//JSON object of post
+    let postsDiv = document.getElementById("Posts");//Get id of div of all posts
+    let newDiv = document.createElement("div");//Create new div for this post
+    let topRowDiv=document.createElement("div");//And one for top row (author + date)
+    topRowDiv.classList.add("topRow");//Add the right class to it
     
     newDiv.appendChild(topRowDiv);
-    let topRowaTag=document.createElement("a");
-    topRowaTag.classList.add("postericon");
-    let topRowPicture=document.createElement("img");
+    let topRowaTag=document.createElement("a");//Creating element for author icon
+    topRowaTag.classList.add("postericon");//Add the right class to it
+    let topRowPicture=document.createElement("img");//Inside it lets create an img tag
     topRowPicture.setAttribute("src","res/images/icon.jpg");
-    topRowaTag.appendChild(topRowPicture);
+    topRowaTag.appendChild(topRowPicture);//Add img tag inside a tag
     topRowDiv.appendChild(topRowaTag);
-    let dateElement=document.createElement("p");
-    dateElement.innerText=postJSON.date;
+    let dateElement=document.createElement("p");//Lets create date element
+    dateElement.innerText=postJSON.date;//Add the date from JSON object as its text
     topRowDiv.appendChild(dateElement);
-    newDiv.appendChild(topRowDiv);
+    newDiv.appendChild(topRowDiv);//Add top row to post div
     
-    if("pictureLink" in postJSON){
-        newDiv.classList.add("PostPhoto");
-        let pictureElement=document.createElement("img");
-        pictureElement.setAttribute("src",postJSON.pictureLink);
-        pictureElement.classList.add("contentpicture");
+    if("pictureLink" in postJSON){//If link to picture in JSON
+        newDiv.classList.add("PostPhoto");//Then its a post with a photo
+        let pictureElement=document.createElement("img");//Create an img tag
+        pictureElement.setAttribute("src",postJSON.pictureLink);//With the right image, src from JSON
+        pictureElement.classList.add("contentpicture");//Add photo class to this picture
         newDiv.appendChild(pictureElement);
-    }else{
+    }else{//Otherwise we have only text post
         newDiv.classList.add("PostText");
     }
 
-    let textElement=document.createElement("p");
-    textElement.innerText=postJSON.text;
+    let textElement=document.createElement("p");//Every post has text so we need a paragraph element
+    textElement.innerText=postJSON.text;//With text from JSON
     newDiv.appendChild(textElement);
 
-    let likeElement=document.createElement("a");
-    likeElement.classList.add("likeicon");
+    let likeElement=document.createElement("a");//Every post also has like icon
+    likeElement.classList.add("likeicon");//Add the right class to it
     let likePicture=document.createElement("img");
-    likePicture.setAttribute("src","res/images/like.png");
+    likePicture.setAttribute("src","res/images/like.png");//And choose the correct icon
     likeElement.appendChild(likePicture);
     newDiv.appendChild(likeElement);
     postsDiv.appendChild(newDiv);
 }
 
-
-let logoA=document.querySelector(".logo");
-let newDiv=document.createElement("div");
-newDiv.id="dropDown";
-let nameP=document.createElement("p");
-nameP.innerText="John Doe";
-let emailP=document.createElement("p");
+//JSON to append Dropdown menu to page HTML
+let logoA=document.querySelector(".logo");//Select user icon on top right of the screen
+let newDiv=document.createElement("div");//Lets create a new div element after it
+newDiv.id="dropDown";//It has certain ID, so its formated correctly with CSS
+let nameP=document.createElement("p");//Add username as first row
+nameP.innerText="John Doe";//Values are hardcoded, but also could easily be fetched from external source like posts
+let emailP=document.createElement("p");//Then email address
 emailP.innerText="john.doe@ut.ee";
-let logoutP=document.createElement("p");
+let logoutP=document.createElement("p");//And lastly logout
 logoutP.innerText="logout";
-newDiv.appendChild(nameP);
+newDiv.appendChild(nameP);//Add all rows to this div
 newDiv.appendChild(emailP);
 newDiv.appendChild(logoutP);
-logoA.after(newDiv);
+logoA.after(newDiv);//And lets add div elemnt after logo
 
-document.querySelector(".logo img").addEventListener('click',toggleDropDown);
-toggleDropDown();
+document.querySelector(".logo img").addEventListener('click',toggleDropDown);//Lets add a listener to activate toggling when user click
+toggleDropDown();//Lets run it one time to hide the dropdown at first
 
 if(document.querySelector("#Posts")){
     fetch("res/posts.json")
